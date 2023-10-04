23f78eab027f16ce53a811e127994fa08c53d300
Lord Basil <126736516+Drlordbasil@users.noreply.github.com>
2023-10-04 12:09:38 -0400

    Update main.py
    improvements.

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
 import requests
 from bs4 import BeautifulSoup
 
+class HttpRequest:
+    def __init__(self, base_url):
+        self.base_url = base_url
+        self.session = requests.Session()
+
+    def get(self, endpoint, params=None):
+        try:
+            response = self.session.get(f"{self.base_url}{endpoint}", params=params)
+            response.raise_for_status()
+            return response
+        except requests.RequestException as e:
+            print(f"An error occurred: {e}")
+            return None
 
 class UserRegistration:
+    """Handles user registration including name, age, and preferred genres."""
     def __init__(self):
         self.name = None
         self.age = None
@@ -14,8 +28,8 @@ class UserRegistration:
         self.preferred_genres = preferred_genres
         print("User registration successful.")
 
-
 class BookRecommendation:
+    """Provides book recommendations based on genre."""
     def __init__(self):
         self.genres = {
             "fiction": [
@@ -42,8 +56,8 @@ class BookRecommendation:
         else:
             return "No recommendations found for the specified genre."
 
-
 class UserFeedback:
+    """Collects and stores user feedback on books."""
     def __init__(self):
         self.feedback = {}
 
@@ -51,81 +65,84 @@ class UserFeedback:
         self.feedback[book] = rating
         print("Thank you for your feedback!")
 
-
-class BookDetails:
-    def __init__(self):
-        self.base_url = "https://www.goodreads.com"
+class BookDetails(HttpRequest):
+    """Fetches book details from Goodreads."""
+    def __init__(self, api_key):
+        super().__init__("https://www.goodreads.com")
+        self.api_key = api_key
 
     def get_book_details(self, book):
-        search_url = (
-            f"{self.base_url}/search/index.xml?key=YOUR_GOODREADS_API_KEY&q={book}"
+        response = self.get(
+            "/search/index.xml",
+            params={"key": self.api_key, "q": book}
         )
-        response = requests.get(search_url)
-        soup = BeautifulSoup(response.content, "xml")
-        best_book = soup.find("best_book")
 
-        if best_book:
-            author = (
-                best_book.find("name").text.strip() if best_book.find("name") else "N/A"
-            )
-            description = (
-                best_book.find("description").text.strip()
-                if best_book.find("description")
-                else "N/A"
-            )
-            average_rating = (
-                best_book.find("average_rating").text.strip()
-                if best_book.find("average_rating")
-                else "N/A"
-            )
+        if response:
+            soup = BeautifulSoup(response.content, "xml")
+            best_book = soup.find("best_book")
 
-            return {
-                "Title": book,
-                "Author": author,
-                "Description": description,
-                "Average Rating": average_rating,
-            }
+            if best_book:
+                author = (
+                    best_book.find("name").text.strip() if best_book.find("name") else "N/A"
+                )
+                description = (
+                    best_book.find("description").text.strip()
+                    if best_book.find("description")
+                    else "N/A"
+                )
+                average_rating = (
+                    best_book.find("average_rating").text.strip()
+                    if best_book.find("average_rating")
+                    else "N/A"
+                )
+
+                return {
+                    "Title": book,
+                    "Author": author,
+                    "Description": description,
+                    "Average Rating": average_rating,
+                }
+            else:
+                return "Book details not found."
         else:
-            return "Book details not found."
+            return "Failed to retrieve book details."
 
-
-class LatestReleases:
-    def __init__(self):
-        self.base_url = "https://api.nytimes.com/svc/books/v3/lists/overview.json"
-        self.api_key = "YOUR_NYTIMES_API_KEY"
+class LatestReleases(HttpRequest):
+    """Fetches the latest book releases from NYTimes."""
+    def __init__(self, api_key):
+        super().__init__("https://api.nytimes.com/svc/books/v3/lists/overview.json")
+        self.api_key = api_key
 
     def get_latest_releases(self):
-        response = requests.get(self.base_url, params={"api-key": self.api_key})
-        if response.status_code == 200:
+        response = self.get("", params={"api-key": self.api_key})
+        if response:
             data = response.json()
             books = [book["title"] for book in data.get("results", {}).get("lists", [])]
             return books
         else:
             return "Failed to retrieve latest releases."
 
-
-class PopularAuthors:
-    def __init__(self):
-        self.base_url = "https://www.goodreads.com/author/featured"
-        self.api_key = "YOUR_GOODREADS_API_KEY"
+class PopularAuthors(HttpRequest):
+    """Fetches popular authors from Goodreads."""
+    def __init__(self, api_key):
+        super().__init__("https://www.goodreads.com/author/featured")
+        self.api_key = api_key
 
     def get_popular_authors(self):
-        author_url = f"{self.base_url}?key={self.api_key}"
-        response = requests.get(author_url)
-        soup = BeautifulSoup(response.content, "html.parser")
-
-        popular_authors = soup.find_all("img", class_="authorCover")
-        authors = [author.get("alt").strip() for author in popular_authors]
-        return authors
-
+        response = self.get("", params={"key": self.api_key})
+        if response:
+            soup = BeautifulSoup(response.content, "html.parser")
+            popular_authors = soup.find_all("img", class_="authorCover")
+            authors = [author.get("alt").strip() for author in popular_authors]
+            return authors
+        else:
+            return "Failed to retrieve popular authors."
 
 def main():
     registration = UserRegistration()
-
     name = "John Doe"
     age = 25
     preferred_genres = ["mystery", "fantasy"]
-
     registration.register_user(name, age, preferred_genres)
 
     recommendation = BookRecommendation()
@@ -138,18 +155,17 @@ def main():
     rating = 5
     feedback.receive_feedback(book, rating)
 
-    book_details = BookDetails()
+    book_details = BookDetails(api_key="YOUR_GOODREADS_API_KEY")
     book_info = book_details.get_book_details(book)
     print("Book details:", book_info)
 
-    releases = LatestReleases()
+    releases = LatestReleases(api_key="YOUR_NYTIMES_API_KEY")
     latest_books = releases.get_latest_releases()
     print("Latest book releases:", latest_books)
 
-    authors = PopularAuthors()
+    authors = PopularAuthors(api_key="YOUR_GOODREADS_API_KEY")
     popular_authors = authors.get_popular_authors()
     print("Popular authors:", popular_authors)
-
 
 if __name__ == "__main__":
     main()
